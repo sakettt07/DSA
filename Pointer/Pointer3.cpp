8e1cac174ebc06e77b601f54e88abec6f59b0634
saket <saket@saketgupta.com>
2023-08-03 18:30:46 +0530

    Double pointer

--- Pointer/Pointer3.cpp
+++ Pointer/Pointer3.cpp
@@ -1,7 +1,20 @@
-#include<bits/stdc++.h>
+// in this we are going to learn about double pointer (not so an important topic);
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
+    int i = 20;
+    int *p = &i;
+    int **p2 = &p;
+    cout<<"running good"<<endl;
+    cout<<"printing p: "<<p<<endl;
+    cout<<" address of p: "<<&p<<endl;
+    cout<<*p2<<endl;
 
-return 0;
+    cout<<"printing i: "<<i<<endl;
+    cout<<"printing i: "<<*p<<endl;
+    cout<<"printing i: "<<**p2<<endl;
+
+    return 0;
 }
