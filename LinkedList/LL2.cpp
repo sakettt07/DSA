5ec690e4a97a28756e0dec1339e4a75cb31995d1
saket <saket@saketgupta.com>
2023-08-19 20:03:56 +0530

    Insertion in DoublyLL

--- LinkedList/LL2.cpp
+++ LinkedList/LL2.cpp
@@ -18,7 +18,7 @@ class Node{
 void print(Node* head){
     Node* temp=head;
     while(temp != NULL){
-        cout<<temp->data;
+        cout<<temp->data<<" ";
         temp=temp->next; 
     }
     cout<<endl;
@@ -33,9 +33,65 @@ int getlength(Node* head){
     }
     return len;
 }
+void insertAtHead(Node* &head,int d ){
+    if(head==NULL){
+        Node* temp=new Node(d);
+        head=temp;
+
+    }
+    else{
+        Node* temp=new Node(d);
+    temp->next=head;
+    head->prev= temp->next;
+    head=temp;
+    }
+}
+void insertAtTail(Node* &tail,int d){
+    Node* temp=new Node(d);
+    tail->next=temp;
+    temp->prev=tail;
+    tail=temp;
+    
+}
+void insertAtPosi(Node* & tail,Node* & head,int position,int d){
+    if(position==1){
+        insertAtHead(head,d);
+        return;
+    }
+    Node *temp = head;
+    int cnt = 1;
+    while (cnt < position - 1)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+    if(temp->next==NULL){
+        insertAtTail(tail,d);
+        return;
+    }
+    Node *nodetoinsert = new Node(d);
+    nodetoinsert->next = temp->next;
+    temp->next->prev = nodetoinsert;
+    temp->next=nodetoinsert;
+    nodetoinsert->prev=temp;
+}
 int main(){
     Node* node1=new Node(10);
     Node* head=node1;
+    Node* tail=node1;
+    print(head);
+    // cout<<getlength(head)<<endl;
+
+    insertAtHead(head,11);
+    print(head);
+    insertAtHead(head,12);
+    print(head);
+    insertAtHead(head,13);
+    print(head);
+
+    insertAtTail(tail,15);
+    print(head);
+    insertAtPosi(tail,head,3,24);
     print(head);
 
 return 0;
