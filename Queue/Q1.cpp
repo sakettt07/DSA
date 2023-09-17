74f395a5d82c31f9d3888feda2e0f85830f554cc
saket <saket@saketgupta.com>
2023-09-17 21:01:09 +0530

    Queue

---
+++ Queue/Q1.cpp
@@ -0,0 +1,9 @@
+// Starting off with the queue data structure
+// Introduction - works on the principle of first in first out (FIFO)
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+return 0;
+}
