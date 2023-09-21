e60357252843ac0edbb7176b73e4d465c9494c80
saket <saket@saketgupta.com>
2023-09-21 19:49:03 +0530

    Q implementation ,CircularQ

---
+++ Queue/CircularQue.cpp
@@ -0,0 +1,23 @@
+// Implementing circular queue->
+
+/* Theory - It is an extension of queue in which when we perform operations like push pop then we move to n so if we have the blank spaces remaining at the starting then our rear will move to the empty part so that the space will not be left used.*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+class circularQ{
+    int *arr;
+    int front;
+    int rear;
+    int size;
+    public:
+    circularQ(int n){
+
+    }
+
+};
+
+int main(){
+
+return 0;
+}
--- Queue/Q1.cpp
+++ Queue/Q1.cpp
@@ -1,13 +1,96 @@
 // Starting off with the queue data structure
 // Introduction - works on the principle of first in first out (FIFO)
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+// Implementing queue using arrays:
+
+class queue{
+    int* arr;
+    int qfront;
+    int rear;
+    int size;
+    public:
+    queue(){
+        size=10001;
+        arr=new int[size];
+        qfront=0;
+        rear=0;
+
+    }
+    bool isEmpty(){
+        if(qfront==rear){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+    void enqueue(int data){
+        if(rear==size){
+            cout<<"Queue is full "<<endl;
+        }
+        else{
+            arr[rear]=data;
+            rear++;
+        }
+    }
+    int dequeue(){
+        if(qfront==rear){
+            return -1;
+        }
+        else{
+            int ans=arr[qfront];
+            arr[qfront]=-1;
+            qfront++;
+            if(qfront==rear){
+                qfront=0;
+                rear=0;
+            }
+            return ans;
+        }
+    }
+    int front(){
+        if(qfront==rear){
+            return -1;
+        }
+        else{
+            return arr[qfront];
+
+        }
+    }
+
+
+};
+
+
+
+
+
+int main()
+{
 
     // create a queue
-    queue<int>q;
+
+    // performing stl operations n queue.
+    // queue<int> q;
+    // q.push(11);
+    // q.push(14);
+    // q.push(15);
+    // cout << "size of queue is :" << q.size() << endl;
+    // q.pop();
+    // cout << "size of queue is :" << q.size() << endl;
+    // if (q.empty())
+    // {
+    //     cout << "queue is empty " << endl;
+    // }
+    // else
+    // {
+    //     cout << "queue is not empty " << endl;
+    // }
 
 
-return 0;
+
+    return 0;
 }
