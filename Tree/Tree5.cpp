6bf44b2d93983ff5bdf7ef0718e19357264ca4bb
saket <saket@saketgupta.com>
2023-10-15 00:15:59 +0530

    Buring-Tree code

--- Tree/Tree5.cpp
+++ Tree/Tree5.cpp
@@ -2,6 +2,95 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Solution {
+  public:
+    int minTime(Node* root, int target) 
+    {
+        // Your code goes here
+        unordered_map<Node *,Node *>mp;
+        queue<Node *>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            
+                Node * temp=q.front();
+                q.pop();
+                if(temp->left)
+                {
+                    mp[temp->left]=temp;
+                    q.push(temp->left);
+                }
+                if(temp->right)
+                {
+                    mp[temp->right]=temp;
+                    q.push(temp->right);
+                }
+        }
+        Node * targetnode;
+        while(!q.empty())
+        {
+            q.pop();
+        }
+        q.push(root);
+        while(!q.empty())
+        {
+            Node * temp=q.front();
+            q.pop();
+            if(temp->data==target)
+            {
+                targetnode=temp;
+                break;
+            }
+            if(temp->left)
+            {
+                q.push(temp->left);
+            }
+            if(temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+        while(!q.empty())
+        {
+            q.pop();
+        }
+        q.push(targetnode);
+        int sec=0;
+        unordered_map<Node *,bool>vis;
+        vis[targetnode]=true;
+        while(!q.empty())
+        {
+            int n=q.size();
+            for(int i=0;i<n;i++)
+            {
+                Node * temp=q.front();
+                q.pop();
+                if(temp->left && !vis[temp->left])
+                {
+                    q.push(temp->left);
+                    vis[temp->left]=true;
+                    
+                }
+                if(temp->right && !vis[temp->right])
+                {
+                    q.push(temp->right);
+                    vis[temp->right]=true;
+                    
+                }
+                
+                if(mp[temp] && !vis[mp[temp]])
+                {   
+                    q.push(mp[temp]);
+                    vis[mp[temp]]=true;
+                    
+                }
+                
+            }
+            ++sec;
+        }
+        return sec-1;
+    }
+};
 int main(){
 
 return 0;
