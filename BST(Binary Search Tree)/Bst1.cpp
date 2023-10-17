17c5cd6cafca1a4e38a5c78669ae35c341222c1d
saket <saket@saketgupta.com>
2023-10-17 23:59:24 +0530

    BST implementation

--- BST(Binary Search Tree)/Bst1.cpp
+++ BST(Binary Search Tree)/Bst1.cpp
@@ -2,7 +2,76 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Node{
+    public:
+    int data;
+    Node*left;
+    Node*right;
+
+    Node(int d){
+        this->data=d;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+void levelOrder(Node *root)
+{
+    queue<Node *> qe;
+    qe.push(root);
+    qe.push(NULL);
+    while (!qe.empty())
+    {
+        Node *temp = qe.front();
+
+        qe.pop();
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!qe.empty())
+            {
+                qe.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left)
+            {
+                qe.push(temp->left);
+            }
+            if (temp->right)
+            {
+                qe.push(temp->right);
+            }
+        }
+    }
+}
+Node* insertBST(Node* root,int d){
+    if(root==NULL){
+        root=new Node(d);
+        return root;
+    }
+    if(d>root->data){
+        root->right=insertBST(root->right,d);
+    }
+    else{
+         root->left=insertBST(root->left,d); 
+    }
+}
+void takeInput(Node* &root){
+    int data;
+    cin>>data;
+    while(data!=-1){
+     root =insertBST(root,data);
+        cin>>data;
+    }
+}
 int main(){
+    Node* root=NULL;
+    cout<<"Enter data to create BST"<<endl;
+    takeInput(root);
+    cout<<"Printing the BST"<<endl;
+    levelOrder(root);
 
 return 0;
 }
