e93477a0b37f5057b46c99ae3efeb1b604fab52c
saket <saket@saketgupta.com>
2023-10-29 12:37:47 +0530

    heap-sort,heap-stl

--- Heaps/h1.cpp
+++ Heaps/h1.cpp
@@ -64,15 +64,25 @@ void heapify(int arr[],int n,int i){
     int largest=i;
     int left=2*i;
     int right=2*i+1;
-    if(left<n && arr[largest] <arr[left]){
+    if(left<=n && arr[largest] <arr[left]){
         largest=left;
     }
-    if(right<n && arr[largest] <arr[right]){
+    if(right<=n && arr[largest] <arr[right]){
         largest=right;
     }
     if(largest !=i){
         swap(arr[largest],arr[i]);
         heapify(arr,n,largest);
+    }
+}
+
+void heapSort(int arr[],int n){
+    int size=n;
+    while(size>1){
+        // swap kardo pahle or last element ko
+        swap(arr[size],arr[1]);
+        size--;
+        heapify(arr,size,1);
     }
 }
 int main(){
@@ -96,6 +106,36 @@ int main(){
     for(int i=1;i<=n;i++){
         cout<<arr[i]<<" ";
     }cout<<endl;
+
+    // heapsort wala function
+    heapSort(arr,n);
+        cout<<"Printing sorted array"<<endl;
+    for(int i=1;i<=n;i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+
+    // stlll methodd
+    cout<<"Using the priority queue"<<endl;
+    priority_queue<int>pq;
+    pq.push(4);
+    pq.push(2);
+    pq.push(6);
+    pq.push(3);
+    cout<<"element at the top"<<pq.top()<<endl;
+    pq.pop();
+    cout<<"element at the top"<<pq.top()<<endl;
+    cout<<"the size of the priority queue is"<<pq.size()<<endl;
+
+    // for min heap
+    priority_queue<int,vector<int>,greater<int>>minheap;
+    minheap.push(4);
+    minheap.push(2);
+    minheap.push(6);
+    minheap.push(3);
+    cout<<"element at the top"<<minheap.top()<<endl;
+    minheap.pop();
+    cout<<"element at the top"<<minheap.top()<<endl;
+    cout<<"the size of the priority queue is"<<minheap.size()<<endl;
 
 
 return 0;
