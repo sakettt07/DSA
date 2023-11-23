eb682968bdcd456ef006a95685e4a76219d75347
saket <saket@saketgupta.com>
2023-11-23 20:52:35 +0530

    Implementation-Trie

--- Trie/Trie-1.cpp
+++ Trie/Trie-1.cpp
@@ -1,9 +1,89 @@
 // Starting off with the new data structures.
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    cout<<"Hello Trie"<<endl;
+class TrieNode
+{
+public:
+    char data;
+    TrieNode *children[26];
+    bool isTerminal;
+    TrieNode(char ch)
+    {
+        data = ch;
+        for (int i = 0; i < 26; i++)
+        {
+            children[i] = NULL;
+        }
+        isTerminal = false;
+    }
+};
+class Trie
+{
+public:
+    TrieNode *root;
+    Trie()
+    {
+        root = new TrieNode('\0');
+    }
+    void insertUtil(TrieNode *root, string word)
+    {
+        if (word.length() == 0)
+        {
+            root->isTerminal = true;
+            return;
+        }
+        // assumption that word is in capital letter
+        int index = word[0] - 'A';
+        TrieNode *child;
 
-return 0;
+        // present wala case
+        if (root->children != NULL)
+        {
+            child = root->children[index];
+        }
+        else
+        {
+            child = new TrieNode(word[0]);
+            root->children[index] = child;
+        }
+        insertUtil(child, word.substr(1));
+    }
+    void insertWord(string word)
+    {
+        insertUtil(root, word);
+    }
+    bool searchUtil(TrieNode *root, string word)
+    {
+        if (word.length() == 0)
+        {
+            return root->isTerminal;
+        }
+        int index = word[0] - 'A';
+        TrieNode *child;
+        if (root->children[index] != NULL)
+        {
+            child = root->children[index];
+        }
+        else
+        {
+            return false;
+        }
+        return searchUtil(child, word.substr(1));
+    }
+    bool search(string word)
+    {
+        return searchUtil(root, word);
+    }
+};
+int main()
+{
+    // cout << "Hello Trie" << endl;
+    Trie *t = new Trie();
+    t->insertWord("ARM");
+    cout<<"New Word Inserted"<<endl;
+    cout<<endl;
+    cout << "Present or not" << t->search("ARM") << endl;
+
+    return 0;
 }
