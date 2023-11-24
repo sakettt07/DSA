217afbcb3cb90211eea5995b58ad4e46a8cee6a9
saket <saket@saketgupta.com>
2023-11-24 19:58:26 +0530

    Removing word in Trie

--- Trie/Trie-1.cpp
+++ Trie/Trie-1.cpp
@@ -75,6 +75,66 @@ public:
     {
         return searchUtil(root, word);
     }
+    bool hasNoChildren(TrieNode *node)
+    {
+        for (int i = 0; i < 26; i++)
+        {
+            if (node->children[i] != NULL)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Recursive function to remove a word from the Trie
+    TrieNode *removeUtil(TrieNode *root, string word, int depth = 0)
+    {
+        // Base case: If the Trie is empty
+        if (root == NULL)
+        {
+            return NULL;
+        }
+
+        // If we have not reached the end of the word
+        if (depth < word.length())
+        {
+            int index = word[depth] - 'A';
+            root->children[index] = removeUtil(root->children[index], word, depth + 1);
+
+            // If the current node is not part of any other word and has no children, delete it
+            if (root->isTerminal == false && hasNoChildren(root))
+            {
+                delete root;
+                root = NULL;
+            }
+        }
+        else // If the end of the word is reached
+        {
+            // If the current node is part of the word
+            if (root->isTerminal)
+            {
+                root->isTerminal = false;
+            }
+
+            // If the current node has no children, delete it
+            if (hasNoChildren(root))
+            {
+                delete root;
+                root = NULL;
+            }
+        }
+
+        return root;
+    }
+
+    // Public function to remove a word from the Trie
+    void removeWord(string word)
+    {
+        root = removeUtil(root, word);
+    }
+
+    
 };
 int main()
 {
@@ -84,6 +144,10 @@ int main()
     cout<<"New Word Inserted"<<endl;
     cout<<endl;
     cout << "Present or not" << t->search("ARM") << endl;
+
+     t->removeWord("ARM");
+    cout << "Word 'ARM' removed" << endl;
+    cout << "Present or not: " << t->search("ARM") << endl;
 
     return 0;
 }
