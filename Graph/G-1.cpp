5fabb2b105214de49d63bc964bc37a5b9b23da05
saket <saket@saketgupta.com>
2023-12-13 00:06:58 +0530

    Graph DS

---
+++ Graph/G-1.cpp
@@ -0,0 +1,12 @@
+// Starting off with the new data structues which is graph
+// A graph is a combination of nodes and edges
+// Graphs are of two types directed and un directed -->In directed graph the direct of each node is defined and in undirected direction is not defined it can move in any directed it is connected to.
+// Node-An entity to store the data
+// Edges-Connecting the nodes.
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+return 0;
+}
