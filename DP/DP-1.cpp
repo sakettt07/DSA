0bd21119d97a3fecf78b525632fe9189df9db8f1
saket <saket@saketgupta.com>
2024-01-16 23:56:28 +0530

    DP-first time

--- DP/DP-1.cpp
+++ DP/DP-1.cpp
@@ -3,8 +3,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    cout<<"Testing the file is running or not";
+#include<bits/stdc++.h>
+using namespace std;
 
-return 0;
+// the code written below is the top down - Recursion +memoization Dp approach.
+int fib(int n,vector<int> &dp){
+        if(n<=1){
+                return n;
+        }
+        if(dp[n]!=-1){
+                return dp[n];
+        }
+        dp[n]= fib(n-1,dp)+fib(n-2,dp);
+        return dp[n];
+}
+int main()
+{
+        int n;
+        cin>>n;
+        vector<int>dp(n+1);
+        for(int i=0;i<=n;i++) dp[i]=-1;
+
+        cout<< fib(n,dp)<<endl;
+        return 0;
 }
