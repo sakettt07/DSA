db2706d19708c9e32d60742a869ea016674a3778
saket <saket@saketgupta.com>
2024-01-21 23:43:37 +0530

    house robber-2

--- DP/DP-3.cpp
+++ DP/DP-3.cpp
@@ -51,6 +51,33 @@ int solve(vector<int>& nums,int i,int n){
     }
 
 };
+
+// house robber 2
+class Solution {
+public:
+int dp[101];
+int solve(vector<int>& nums,int i,int n){
+    if(i>n)return 0;
+    if(dp[i]!=-1){
+        return dp[i];
+    }
+    int steal=nums[i]+solve(nums,i+2,n);
+    int skip=solve(nums,i+1,n);
+    return dp[i]= max(steal,skip);
+}
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)return nums[0];
+        if(n==2) return max(nums[0],nums[1]);
+        memset(dp,-1,sizeof(dp));
+
+        // taking up the first case in which we will be taking the 0th house.
+        int take_house=solve(nums,0,n-2);
+        memset(dp,-1,sizeof(dp));
+        int not_take=solve(nums,1,n-1);
+        return max(take_house,not_take);
+    }
+};
 int main(){
 
 return 0;
