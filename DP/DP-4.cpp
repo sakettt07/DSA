f6db8f7e1efca873caffd7dfde6b603883ddf7e3
saket <saket@saketgupta.com>
2024-01-22 23:59:53 +0530

    question dp

---
+++ DP/DP-4.cpp
@@ -0,0 +1,33 @@
+// coin change-2 question
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+// DP standard Problem
+
+// Trying the recursion approach first (This will cause runtime error)
+int n;
+// now adding the memoization code into the same.
+int dp[301][5001];
+int solve(int i,int amt,vector<int>& coins){
+    if(amt==0) return 1;
+    if(i==n) return 0;
+    if(dp[i][amt]!=-1)return dp[i][amt];
+    if(amt<coins[i]){
+        return dp[i][amt]=solve(i+1,amt,coins);
+    }
+    int take=solve(i,amt-coins[i],coins);
+    int skip=solve(i+1,amt,coins);
+    return dp[i][amt]= take+skip;
+
+}
+    int change(int amount, vector<int>& coins) {
+        n=coins.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(0,amount,coins);
+    }
+};
+int main(){
+
+return 0;
+}
