d9a99b3d1b9f29e592d1e4f55cab6b40ae59b684
saket <saket@saketgupta.com>
2024-04-13 23:55:56 +0530

    cycle detection in graph

---
+++ Graph/G-4.cpp
@@ -0,0 +1,46 @@
+// Cycle detection in undirected graph using BFS ,DFS.
+#include<bits/stdc++.h>
+using namespace std;
+bool isCycleBFS(int src,unordered_map<int,bool>&visited,unordered_map<int,list<int>> &adj){
+    unordered_map<int,int>parent;
+    parent[src]=-1;
+    visited[src]=1;
+    queue<int>qu;
+    qu.push(src);
+    while(!qu.empty()){
+        
+    }
+
+}
+string cycleDetection (vector<vector<int>>& edges, int n, int m)
+{
+    // Write your code here.
+    // creating the adjacency list for the BFS traversal.
+    unordered_map<int,list<int>>adj;
+     for (int i = 0; i < m; i++)
+    {
+        int u = edges[i][0];
+        int v = edges[i][1];
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    // to handle the disconnected components
+    unordered_map<int,bool>visited;
+
+     for(int i=0;i<n;i++){
+        if (!visited[i])
+        {
+            bool ans=isCycleBFS(i,visited,adj);
+            if(ans==1){
+                return "Yes";
+            }
+        }
+    }
+    return "No";
+}
+
+
+int main(){
+
+return 0;
+}
