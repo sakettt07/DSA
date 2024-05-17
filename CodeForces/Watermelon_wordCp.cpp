7fcf4d3a9917786afb4526d641253b111e5ce2b5
saket <saket@saketgupta.com>
2024-05-18 00:28:12 +0530

    codeforces questions

---
+++ CodeForces/Watermelon_wordCp.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<((n%2==0 && n>2) ? "YES":"NO");
+    string n;
+    cin>>n;
+    n[0]=toupper(n[0]);
+    cout<<n;
+return 0;
+}
