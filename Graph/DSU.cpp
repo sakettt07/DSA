412bb45dcfac4687fdd8ea0eb6196336ec22a785
saket <saket@saketgupta.com>
2024-07-20 23:49:21 +0530

    DSU

--- Graph/DSU.cpp
+++ Graph/DSU.cpp
@@ -1,5 +1,6 @@
 // Disjoint set union new graph topic
 // Disjoint sets are those when the intersection is taken it comes out to be phi or NULL we can say in codingg terms.
+// Done with the theory part
 #include<bits/stdc++.h>
 using namespace std;
 
