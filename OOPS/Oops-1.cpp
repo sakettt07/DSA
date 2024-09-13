c3ba4950f75d00eb58872725b3e4d72c89502278
saket <saket@saketgupta.com>
2024-09-14 01:52:51 +0530

    study about the constructor

--- OOPS/Oops-1.cpp
+++ OOPS/Oops-1.cpp
@@ -1,4 +1,7 @@
 // Starting off with the object oriented programming for the interview process as so many of the companies are asking for it in the apptitude also.
+
+
+// Usage of the this pointer :Points to the current object and avoid the confusion between the compiler.
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -6,13 +9,39 @@ class Teacher{
 // Implementing the access modifier.
 // By default it is private in nature.
     private:
-    double salary=60000;
+    double salary;
 
 
     public:
-    // properties
+        // properties
     string name;
     string dept;
+    string subject;
+
+    // constructor
+    // Non parameterized 
+    Teacher(){
+        dept="Inforamtion Technology.";
+        cout<<"The constructor is called."<<endl;
+    }
+
+    // Parameterized
+    Teacher(string n,string d,string s,double sal){
+        this->name=n;
+        this->dept=d;
+        this->subject=s;
+        this->salary=sal;
+    }
+
+    // Custom copy constructor:
+    Teacher(Teacher &obj){
+        cout<<"I am the default copy constructor...."<<endl;
+        this->name=obj.name;
+        this->dept=obj.dept;
+        this->subject=obj.subject;
+        this->salary=obj.salary;
+    }
+
 
     // methods
     void changeDept(string newDept){
@@ -26,17 +55,27 @@ class Teacher{
     double getSalary(){
         return salary;
     }
+    // to get the information of the teacher.
+    void getInfo(){
+        cout<<"name:"<<name<<endl;
+        cout<<"Dept:"<<dept<<endl;
+        cout<<"subject:"<<subject<<endl;
+        cout<<"salary:"<<salary;
+    }
 
 };
 
 int main(){
-    Teacher t1;
-    t1.setSalary(20000);
-    t1.getSalary();
-    t1.dept="Computer Science";
-    t1.changeDept("Information technology");
-    cout<<"The department of the teacher is: "<<t1.dept<<endl;
-    cout<<"The salary of the teacher is :"<<t1.getSalary();
+
+    Teacher t1("aman","mechanical","theory of computation",40000);
+    Teacher t2;
+    // t1.setSalary(20000);
+    // t1.changeDept("Information technology");
+    // t1.getInfo();
+    Teacher t3(t1);     //this will be the default copy constructor.
+    t3.getInfo();
+
+
 
 return 0;
 }
