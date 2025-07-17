005201d7fba2780f0b6693ce8b99d3de1f1389a2
saket <saket@saketgupta.com>
2025-07-17 23:56:41 +0530

    Refactor Count_All_Possible_Routes and Max_Value_After_JobScheduling implementations; enhance structure and add missing logic

--- DP/Hard/Count_All_Possible_Routes.cpp
+++ DP/Hard/Count_All_Possible_Routes.cpp
@@ -1,8 +1,15 @@
 // In this file we  have solved a DP problem to count all the possible routes.
+// we are given with the locations , start end, and the fuel we have to travel with.
+// we have to find the total number of routes we can take to reach the destination with the present amount of fuel.
 #include<bits/stdc++.h>
 using namespace std;
 
 class Solution{
+    public:
+    int n;
+    const int MOD=1e9+7;
+    int solve(){}
+    int countRoutesUtil(int start, int end, int fuel, vector<vector<int>>& locations) {}
 
 };
 int main(){
--- DP/Hard/Max_Value_After_JobScheduling.cpp
+++ DP/Hard/Max_Value_After_JobScheduling.cpp
@@ -1,27 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Solution{
-    public:
-    int n;
+class Solution {
+public:
+int n;
+int dp[50001];    // memoizing for the changing index.
+int genNextIdx(vector<vector<int>>& array, int l,int target){
+    int r=n-1;
+    int result=n+1;
+    while(l<=r){
+        int mid=l+(r-l)/2;
+        if(array[mid][0]>=target){  // this means we can take this job as its starting time is greater.
+        result=mid;
+        r=mid-1;
+        }
+        else{
+            l=mid+1;
+        }
+    }
+    return result;
+}
     int solve(vector<vector<int>>& array, int idx) {
         if(idx>=n){
             return 0;
         }
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+        int nextIdx=genNextIdx(array,idx+1,array[idx][1]);
         int notTake=solve(array,idx+1);
-        int take=0;     
-        return max(take,notTake);
+        int take=array[idx][2]+solve(array,nextIdx);     
+        return dp[idx]= max(take,notTake);
     }
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         n=startTime.size();
         vector<vector<int>>array(n,vector<int>(3,0));
-        for(int i=0;i<i<n;i++){
+        memset(dp,-1,sizeof(dp));
+        for(int i=0;i<n;i++){
             array[i][0]=startTime[i];
             array[i][1]=endTime[i];
             array[i][2]=profit[i];
         }
-        auto comp = [](const vector<int>& a, const vector<int>& b) {
-            return a[1] < b[1];
+        auto comp = [&](auto& a1, auto& b1) {
+            return a1[0] < b1[0];
         };
         sort(array.begin(),array.end(),comp);
         return solve(array,0);
