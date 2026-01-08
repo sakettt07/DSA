544f3aacb0b3d92148b3deb86718cf1f118f1730
saket <saket@saketgupta.com>
2026-01-08 23:59:13 +0530

    Add implementation for minimum one bit operations; define minimumOneBitOperations method to calculate operations needed to convert number to zero

---
+++ Bit Manipulation/Min1BitToMake0.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        if(n==0){
+            return 0;
+        }
+        vector<long,long>F(31,0);  // a number can have 32 bits only
+        F[0]=1;
+        //storing all the ith bit set
+        for(int i=1;i<=30;i--){
+            F[i]=2*F[i-1]+1;
+        }
+        int result=0;
+        int sign=1;
+        for(int i=30;i>=0;i--){
+            int ithBit=((1<<i)&n);
+            if(ithBit==0){
+                continue;
+            }
+            if(sign>0){
+                result+=F[i];
+            }
+            else{
+                result-=F[i];
+            }
+            sign=sign*(-1);
+        }
+    }
+};
+int main(){
+
+return 0;
+}
