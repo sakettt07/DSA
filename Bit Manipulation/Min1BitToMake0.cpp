b027ce62f7d5af042755a27127ab14c090d4bcb2
saket <saket@saketgupta.com>
2026-01-10 23:59:48 +0530

    Fix type declaration for vector in minimumOneBitOperations method

--- Bit Manipulation/Min1BitToMake0.cpp
+++ Bit Manipulation/Min1BitToMake0.cpp
@@ -6,10 +6,10 @@ public:
         if(n==0){
             return 0;
         }
-        vector<long,long>F(31,0);  // a number can have 32 bits only
+        vector<long long>F(31,0);  // a number can have 32 bits only
         F[0]=1;
         //storing all the ith bit set
-        for(int i=1;i<=30;i--){
+        for(int i=1;i<=30;i++){
             F[i]=2*F[i-1]+1;
         }
         int result=0;
