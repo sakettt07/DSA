52caffac5cbb0627f519cdea35737e06969bde8b
saket <guptasaket898@gmail.com>
2026-04-08 23:59:48 +0530

    Refactor DSU implementation: streamline Union method and remove unused rank vector

--- Graph/DSU.cpp
+++ Graph/DSU.cpp
@@ -6,30 +6,19 @@ using namespace std;
 
 class Solution {
 public:
-vector<int>rank;
 vector<int>parent;
 int find(int x){
     if(x==parent[x]){
         return x;
     }
-    return parent[x]=find(parent[x]);
+    return find(parent[x]);
 }
 void Union(int x,int y){
     int x_parent=find(x);
     int y_parent=find(y);
 
-    if(x_parent==y_parent){
-        return ;
-    }
-    if(rank[x_parent]>rank[y_parent]){
+    if(x_parent!=y_parent){
         parent[y_parent]=x_parent;
-    }
-    else if(rank[y_parent]>rank[x_parent]){
-        parent[x_parent]=y_parent;
-    }
-    else{
-        parent[x_parent]=y_parent;
-        rank[y_parent]++;
     }
 }
     int makeConnected(int n, vector<vector<int>>& connections) {
@@ -40,8 +29,6 @@ void Union(int x,int y){
         for(int i=0;i<n;i++){
             parent[i]=i;
         }
-
-
     }
 };
 int main(){
--- Graph/DSURank.cpp
+++ Graph/DSURank.cpp
@@ -2,7 +2,36 @@
 using namespace std;
 // in this method we will be using the rank of the parent to decide which one will be the parent of the other one
 
-int main(){
+class Solution {
+    vector<int>parent;
+    vector<int>rank;
+    public:
+    int find(int x){
+    if(x==parent[x]){
+        return x;
+    }
+    return parent[x]=find(parent[x]);
+}
+    int Union(){
+        int x_parent=find(x);
+    int y_parent=find(y);
+    if(x_parent==y_parent){
+        return ;
+    }
+    if(rank[x_parent]>rank[y_parent]){
+        parent[y_parent]=x_parent;
+    }
+    else if(rank[y_parent]>rank[x_parent]){
+        parent[x_parent]=y_parent;
+    }
+    else{
+        parent[x_parent]=y_parent;
+        rank[y_parent]++;
+    }
+    }
+};
 
+int main(){
+    Solution s1;
 return 0;
 }
